44f2550cfb27a70f5ab3ce7e255ca3cc3c0b32b9
jnhustin <jnhustin@gmail.com>
2017-02-02 21:05:47 -0800

    fixed functions, clock now starts at designated times

--- clock.js
+++ clock.js
@@ -3,31 +3,48 @@ document.addEventListener("DOMContentLoaded", function() {
 	var secondsHand = document.getElementById('second');
 	var minsHand = document.getElementById('minute');
 	var hourHand = document.getElementById('hour');
-	var seconds = 50;
-	var minutes = 3;
-	var hours = 0;
+	var seconds = 55;
+	var minutes = 58;
+	var hours = 5;
 	var secsDegree = 0;
 	
-	setInterval(secondsRotation, 1000);
+	minuteUpdate();
+	hourUpdate();
+
+	setInterval(hourUpdate, 1000);
 	setInterval(minuteUpdate, 1000);
+	setInterval(secondsRotation, 1000);
 
-	function minuteUpdate() {
-		if (secsDegree === 0) {
-			var minsRotation = (minutes * 6) % 360;
-			minutes++;
-			minutes %= 60;
-			minsHand.style.transform = 'rotate(' + minsRotation + 'deg)';
 
+	function hourUpdate() {
+		var hourRotation = (5 / 12) * 360;
+		hourHand.style.transform = 'rotate(' + hourRotation + 'deg)';
+		
+		if (minutes === 0) {
+			hour++;
+			hour %= 24;
 		}
 	}
-
+	function minuteUpdate() {
+		var minsRotation = (minutes * 6)% 360;
+		minsHand.style.transform = 'rotate(' + minsRotation + 'deg)';
+		if (seconds === 0) {
+			minutes++;
+			minutes %= 60;
+			console.log(minutes);
+		}
+	}
 	function secondsRotation() {
 		secsDegree = (seconds / 60) * 360 % 360;
 		secondsHand.style.transform = 'rotate(' + secsDegree + 'deg)';
 		seconds++;
 		seconds %= 60;
 	}
-
+	function checkUpdates() {
+		hourUpdate();
+		minuteUpdate();
+		secondsRotation();
+	}
 });
 
 
