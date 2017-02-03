04d61859e90d5338716ee3262ba9f23bb0dd8b9d
jnhustin <jnhustin@gmail.com>
2017-02-02 18:54:57 -0800

    restarted code without functions, got seconds to rotate second hand

--- clock.js
+++ clock.js
@@ -2,38 +2,39 @@ document.addEventListener("DOMContentLoaded", function() {
 	console.log("All DOM content has been loaded.");
 	var secondsHand = document.getElementById('second');
 	var number = 56;
+	var rotation = 0;
+	setInterval(function() {
+		rotation = (number / 60) * 360 % 360;
+		secondsHand.style.transform = 'rotate(' + rotation + 'deg)';
+		number++;
+	}, 1000)
 
 
 
-	
 
-secondRotation(secondsHand);
+// function secondRotation(hand) {
+// 	if (hand) {
+// 		var rotation = (number / 60) * 360 % 360;
+// 		secondsHand.style.transform = 'rotate('+rotation+'deg)';
+// 		console.log(rotation)
+// 	}
+// }
 
-function secondRotation(hand) {
-	if (hand) {
-		var rotation = (number / 60) * 360 % 360;
-		secondsHand.style.transform = 'rotate('+rotation+'deg)';
-		console.log(rotation)
-	}
-}
-
-function secondInterval() {
-	setInterval(rotate(), 1000);
-
-}
-
-function minInterval() {
-
-}
-function sixtySeconds() {
-	secondInterval();
-	setTimeout(clearTimeout(secondInterval), 60000)
-}
-
-
-// function rotate() {
+// function secondInterval(rotation) {
+// 	setInterval(rotation, 1000);
 
 // }
+
+// function minInterval() {
+
+// }
+// function sixtySeconds() {
+// 	secondInterval();
+// 	setTimeout(clearTimeout(secondInterval), 60000)
+// }
+
+
+
 });
 
 
