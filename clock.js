b2f19fe29542dd4d8516865ed7b145afdc242067
jnhustin <jnhustin@gmail.com>
2017-02-02 21:20:50 -0800

    factored out WET code -style.transform

--- clock.js
+++ clock.js
@@ -1,49 +1,49 @@
 document.addEventListener("DOMContentLoaded", function() {
 	console.log("All DOM content has been loaded.");
-	var secondsHand = document.getElementById('second');
+	var secsHand = document.getElementById('second');
 	var minsHand = document.getElementById('minute');
 	var hourHand = document.getElementById('hour');
-	var seconds = 55;
-	var minutes = 58;
+	var secs = 55;
+	var minutes = 59;
 	
 	minuteUpdate();
 	hourUpdate();
 
-	setInterval(hourUpdate, 1000);
-	setInterval(minuteUpdate, 1000);
-	setInterval(secondsRotation, 1000);
-
+	setInterval(checkUpdates, 1000);
 
 	function hourUpdate() {
-		var hours = 5;
-		var hourRotation = (5 / 12) * 360;
-		hourHand.style.transform = 'rotate(' + hourRotation + 'deg)';
-		
 		if (minutes === 0) {
 			hour += 1;
 			hour %= 24;
 		}
+		var hours = 5;
+		var hourRotation = (5 / 12) * 360;
+		rotateHandbyDegree(hourHand, hourRotation);
 	}
 	function minuteUpdate() {
-		var minsRotation = (minutes * 6)% 360;
-		minsHand.style.transform = 'rotate(' + minsRotation + 'deg)';
-		if (seconds === 0) {
+		if (secs === 0) {
 			minutes += 1;
 			minutes %= 60;
 			console.log(minutes);
 		}
+		var minsRotation = (minutes * 6)% 360;
+		rotateHandbyDegree(minsHand, minsRotation);
 	}
-	function secondsRotation() {
-		var secsDegree = (seconds / 60) * 360 % 360;
-		secondsHand.style.transform = 'rotate(' + secsDegree + 'deg)';
-		seconds += 1;
-		seconds %= 60;
+	function secsRotation() {
+		var secsDegree = (secs / 60) * 360 % 360;
+		rotateHandbyDegree(secsHand, secsDegree);
+		secs += 1;
+		secs %= 60;
 	}
 	function checkUpdates() {
 		hourUpdate();
 		minuteUpdate();
-		secondsRotation();
+		secsRotation();
 	}
+	function rotateHandbyDegree(hand, degree) {
+		hand.style.transform = 'rotate(' + degree + 'deg)';
+	}
+
 });
 
 
