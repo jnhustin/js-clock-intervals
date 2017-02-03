ec9aaeff4f49c27ad74a3f5457504f63eeb44764
jnhustin <jnhustin@gmail.com>
2017-02-02 18:45:58 -0800

    got transform rotations working

--- clock.js
+++ clock.js
@@ -1,26 +1,52 @@
 document.addEventListener("DOMContentLoaded", function() {
 	console.log("All DOM content has been loaded.");
-	var timeInSecond = document.getElementById('second');
+	var secondsHand = document.getElementById('second');
+	var number = 56;
 
-	rotate();
 
+
+	
+
+secondRotation(secondsHand);
+
+function secondRotation(hand) {
+	if (hand) {
+		var rotation = (number / 60) * 360 % 360;
+		secondsHand.style.transform = 'rotate('+rotation+'deg)';
+		console.log(rotation)
+	}
+}
+
+function secondInterval() {
+	setInterval(rotate(), 1000);
+
+}
+
+function minInterval() {
+
+}
+function sixtySeconds() {
+	secondInterval();
+	setTimeout(clearTimeout(secondInterval), 60000)
+}
+
+
+// function rotate() {
+
+// }
 });
 
 
 
 
 
-function secondInterval() {
-	setInterval(rotate, 1000);
 
 
-function sixtySeconds() {
-	secondInterval();
-	setTimeout(clearTimeout(secondInterval), 60000)}
-}
-function rotate(clockHand) {
-	if (clockHand) {
-		clockHand.style.transform = 'rotate(100deg)';
-	}
-}
-secondInterval();
+
+
+
+
+
+
+
+
